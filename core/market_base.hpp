f69a108853e513f4a43c526bd3aab2569de33b2e
ekrishgupta <ekrishgupta@users.noreply.github.com>
2026-02-23 23:05:20 -0800

    feat(core): add load_markets interface and ticker mapping to MarketBackend

--- core/market_base.hpp
+++ core/market_base.hpp
@@ -42,8 +42,19 @@ struct MarketBackend {
   virtual std::string name() const = 0;
 
   auth::Credentials credentials;
+  std::map<std::string, std::string> ticker_to_id;
 
   void set_credentials(const auth::Credentials &creds) { credentials = creds; }
+
+  virtual void load_markets() {}
+
+  std::string resolve_ticker(const std::string &ticker) const {
+    auto it = ticker_to_id.find(ticker);
+    if (it != ticker_to_id.end()) {
+      return it->second;
+    }
+    return ticker;
+  }
 
   // --- Exchange & Status ---
   virtual std::string get_exchange_status() const { return "active"; }
