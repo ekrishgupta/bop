dd24c2219c428de91b5e2c0595ba814b15e07100
ekrishgupta <ekrishgupta@users.noreply.github.com>
2026-02-28 21:26:38 -0800

    feat: add backend capabilities and traits for compile-time validation

---
+++ core/capabilities.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <type_traits>
+
+namespace bop {
+
+// Capability Tags
+struct SupportsPostOnly {};
+struct SupportsIceberg {};
+struct SupportsSTP {};
+struct SupportsFOK {};
+struct SupportsIOC {};
+
+// Traits base
+template <typename T> struct BackendTraits {
+  static constexpr bool supports_post_only = false;
+  static constexpr bool supports_iceberg = false;
+  static constexpr bool supports_stp = false;
+  static constexpr bool supports_fok = true; // Default
+  static constexpr bool supports_ioc = true; // Default
+};
+
+// Concepts (C++20)
+template <typename T>
+concept PostOnlyCapable = BackendTraits<T>::supports_post_only;
+
+template <typename T>
+concept IcebergCapable = BackendTraits<T>::supports_iceberg;
+
+template <typename T>
+concept STPCapable = BackendTraits<T>::supports_stp;
+
+} // namespace bop
